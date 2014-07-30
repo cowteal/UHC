f86e2dec985ff97ede25dc56559d342cbaece500
CrazymanJR <adilkarim101@gmail.com>
2014-07-30 12:20:39 -0700

    Update Class_Setup.java

--- src/net/UltraHardCore/Class_Setup.java
+++ src/net/UltraHardCore/Class_Setup.java
@@ -7,15 +7,22 @@ public class Class_Startup extends JavaPlugin  {
 	String author = "CrazymanJR, coldguy101 & darkzonex44";
  
 	
-	 @Override
+	@Override
 	public void onEnable() {
-	   System.out.println("[UHC] Developed by " + author);
+	   getLogger().info("UHC Has Been Enabled!");
+	   getLogger().info("Developed By: " + author);
 	    /*
 	     * Config & More Stuff here!
 	     */
 	     register();
 	     executecommands();
 	}
+	
+	@Override
+	public void onDisable() {
+           getLogger().info("UHC Has Been Disabled!");
+	}
+	
 	  
 	private void register() {
 	  getServer().getPluginManager().registerEvents(new Events(), this);
