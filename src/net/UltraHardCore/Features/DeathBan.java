c3835e6e1c6343540d496aeef2a01e60ed0a255f
CrazymanJR <adilkarim101@gmail.com>
2014-07-30 12:54:50 -0700

    Update DeathBan.java

--- src/net/UltraHardCore/Features/DeathBan.java
+++ src/net/UltraHardCore/Features/DeathBan.java
@@ -1,1 +1,13 @@
-//TODO: Add Code
+package net.UltraHardCore.Features.DeathBan;
+
+public class DeathBan implements Listener {
+  
+  String prefix = ChatColor.GRAY + "[" + ChatColor.RED + "UHC" + ChatColor.GRAY + "] ";
+  
+  @EventHandler
+  public void onPlayerDeath(PlayerDeathEvent event){
+  
+  event.getPlayer().kickPlayer(prefix + ChatColor.AQUA + "You Have Been Defeated!");
+  Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "ban " + player.getName() + prefix + ChatColor.AQUA + "You Have Been Defeated!");
+  }
+}
