46d0f5616cdbf1cae815a706abe50da7ad3f28f6
CrazymanJR <adilkarim101@gmail.com>
2014-07-30 13:02:59 -0700

    Update DeathBan.java

--- src/net/UltraHardCore/Features/DeathBan.java
+++ src/net/UltraHardCore/Features/DeathBan.java
@@ -1,5 +1,10 @@
 package net.UltraHardCore.Features.DeathBan;
 
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
 public class DeathBan implements Listener {
   
   String prefix = ChatColor.GRAY + "[" + ChatColor.RED + "UHC" + ChatColor.GRAY + "] ";
